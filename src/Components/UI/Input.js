4bef764d0d673acaec23fbcb34c3fc40632dd4e1
RahulRajak525 <rahulrajak525@gmail.com>
2022-12-03 00:08:49 +0530

    react Modal via create portal

--- src/Components/UI/Input.js
+++ src/Components/UI/Input.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import classes from './Input.module.css'
+import React from "react";
+import classes from "./Input.module.css";
 
-const Input = props => {
+const Input = (props) => {
   return (
-    <div className={classes.input} >
-      <lebel htmlFor={props.input.id} >{props.label}</lebel>
-       <input id={props.input.id} {...props.input} />
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input id={props.input.id} {...props.input} />
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
